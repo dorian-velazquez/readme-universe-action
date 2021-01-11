a2429e4ac15f8b0736b3107575e3e5956d8d8911
Dorian Velazquez <dorian.velazquez@kroger.com>
2021-01-10 16:18:54 -0800

    testing render

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,27 @@ prod=(stage-centralus prod-centralus)
 
 for environment in $(ls environments)
 do
+	export tf_working_dir="environments/$environment"
+	
 	cd environments/$environment
-	#if [[ " ${prod[@]} " =~ " ${environment} " ]]; then
-		export tf_working_dir="environments/$environment"
-		terragrunt init
-		terragrunt output-all
-	#fi
+	
+	if [[ " ${prod[@]} " =~ " ${environment} " ]]; then
+		export ARM_ACCESS_KEY=PROD_ARM_ACCESS_KEY
+		export ARM_CLIENT_ID=PROD_ARM_CLIENT_ID
+		export ARM_CLIENT_SECRET=PROD_ARM_CLIENT_SECRET
+		export ARM_SUBSCRIPTION_ID=PROD_ARM_SUBSCRIPTION_ID
+		export ARM_TENANT_ID=PROD_ARM_TENANT_ID
+	else
+		export ARM_ACCESS_KEY=NONPROD_ARM_ACCESS_KEY
+		export ARM_CLIENT_ID=NONPROD_ARM_CLIENT_ID
+		export ARM_CLIENT_SECRET=NONPROD_ARM_CLIENT_SECRET
+		export ARM_SUBSCRIPTION_ID=NONPROD_ARM_SUBSCRIPTION_ID
+		export ARM_TENANT_ID=NONPROD_ARM_TENANT_ID
+	fi
+	
+	terragrunt init
+	terragrunt output-all
+	
 	cd ../../
 done
 python /script.py
