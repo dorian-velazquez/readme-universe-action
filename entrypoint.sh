28693e8363d42dc2a97fb2adc211b577c169b306
Dorian Velazquez <dorian.velazquez@kroger.com>
2021-01-12 12:04:00 -0800

    testing stderr to be silent

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,7 @@ export ARM_TENANT_ID=$NONPROD_ARM_TENANT_ID
 export tf_working_dir="environments/dev-centralus"
 cd environments/dev-centralus/01-subnet
 terragrunt output-all -json 2> tee output.json
+cat output.json
 
 : '
 for environment in $(ls environments)
