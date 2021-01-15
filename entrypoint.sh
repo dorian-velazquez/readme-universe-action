53097cb1a4ee85b660d67cbdb6fe3d0ce723f5b6
Dorian Velazquez <dorian.velazquez@kroger.com>
2021-01-15 15:01:29 -0800

    testing commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 echo $(git status)
 echo $(git rev-parse --abbrev-ref HEAD)
 echo $(git remote -v)
+git add README.md
 if [ -z "$(git diff origin/main --  README.md)" ]
 then
-	git add README.md
 	git commit -m "rendered README.md" 
 	git status
 	git push origin $BRANCH_NAME
