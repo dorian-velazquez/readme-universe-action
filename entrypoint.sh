ebc5a65ab771e16aa659979f70fcaf094153ee71
Dorian Velazquez <dorian.velazquez@kroger.com>
2021-01-14 16:03:11 -0800

    create a new branch to avoid conflicts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+BRANCH_NAME="readme"
+
+echo "Branch: [$BRANCH_NAME]"
+
+# checkout the branch, if specified
+if [ "$BRANCH_NAME" != "main" ]; then
+# try to check out the origin, if fails, then create the local branch
+	git fetch && git checkout $BRANCH_NAME && git pull || git checkout -b $BRANCH_NAME
+fi
+
 git add README.md
 git commit -m "rendered README.md" || exit 0
 git status
-git push origin automation
+git push origin develop
